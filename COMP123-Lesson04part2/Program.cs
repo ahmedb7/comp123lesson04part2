c93e936d73edec0041db4dfff676ef3bcea33fa5
Benzir <benzir.ahmed7@gmail.com>
2015-01-27 13:43:35 -0500

    Hero calss01

--- COMP123-Lesson04part2/Program.cs
+++ COMP123-Lesson04part2/Program.cs
@@ -10,6 +10,12 @@ namespace COMP123_Lesson04part2
     {
         static void Main(string[] args)
         {
+            hero myHero = new hero(7, 8, "female");
+            myHero.doesGoodThings();
+
+            //Wait for input
+            Console.WriteLine("Press any key to exit:");
+            Console.ReadKey();
         }
     }
 }
---
+++ COMP123-Lesson04part2/heroClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Lesson04part2
+{
+    class hero
+    {
+        //Private Properties ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        bool bravery = true;
+        private int health;
+        private int smarts;
+        private string gender;
+        //Constructor+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public hero(int health, int smarts, string gender)
+    {
+        this.health = health;
+        this.smarts = smarts;
+        this.gender = gender;
+
+    }
+        public void doesGoodThings()
+        {
+            Console.WriteLine("Doin'good things....YEAH");
+        }
+    }
+}
